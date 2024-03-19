2d56776ce50dcb26b954f264585d64898c21cb8f
EidmantasDev <eidmantas.slenys@gmail.com>
2024-03-19 12:05:26 +0200

    Created Header, ResultsTable, UserInput components In header component added h1, and image In resultstable component added table skeleton In user input component added four inputs with labels Lifted state from user input to app component to be reachable by user input adn results table components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+import Header from './components/Header/Header';
+import ResultsTable from './components/ResultsTable/ResultsTable';
+import UserInput from './components/UserInput/UserInput';
+
+const INITIAL_USER_INPUT = {
+  initialInvestment: 10000,
+  annualInvestiment: 1200,
+  expectedReturn: 6,
+  duration: 10,
+};
+
 function App() {
+  const [userInput, setUserInput] = useState(INITIAL_USER_INPUT);
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChangeInput={handleChange} />
+      <ResultsTable userInput={userInput} />
+    </>
+  );
 }
 
-export default App
+export default App;
